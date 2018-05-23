10a87bfe98c09a15065d8811225fee71dec7acf9
santhosh220897 <31367133+santhosh220897@users.noreply.github.com>
2018-05-23 18:59:36 +0530

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,18 +1,25 @@
 class Address < ApplicationRecord
+	#association has been used because address can have many versions
 	has_many :versions
+	#validate the company name
 	validates_presence_of :company
+	#callbacks has been used
 	after_validation do |address|
 		puts "Validation over"
 	end
+	
+	#before updating the database "addresses" i am adding the previous data in the versions table
 	before_update :add_data, on: [:update]
-
 	private 
 		def add_data
+			#creating new versions tuple with empty values
 			@versions =Version.new
+			#passing the values from addresses table to the versions table
 			@versions.address_id = self.id_was
 			@versions.ver_address = self.address_was
 			@versions.created_at = self.created_at_was
 			@versions.updated_at = self.updated_at_was
+			#the below line is to save the above changes to the verions table
 			@versions.save
 		end
 
@@ -20,6 +27,8 @@ class Address < ApplicationRecord
 
 	private
 	def add_data_more
+		#add the versions id to the addresses table for the clients want to change the current add. to previous add.
+		# add.  means address
 		self.current_version_id = @versions.id
 	end
 end
