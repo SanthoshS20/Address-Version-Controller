c3989f439c0540d7223621557f7ea7baa8f7561a
santhosh220897 <31367133+santhosh220897@users.noreply.github.com>
2018-05-23 19:00:44 +0530

    Update version.rb

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,5 +1,7 @@
 class Version < ApplicationRecord
+    #association has been used. Here version is belongs to address
     belongs_to :address
+    #validation made below
     validates_presence_of :address_id
     validates_presence_of :address
 
