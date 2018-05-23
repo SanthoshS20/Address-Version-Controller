ba5d677d2ea49638217b94bc53b4bfaabee1eafb
santhosh220897 <31367133+santhosh220897@users.noreply.github.com>
2018-05-23 18:49:05 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #routes for my controller has been initialized here
   resources :address
   resources :version
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
